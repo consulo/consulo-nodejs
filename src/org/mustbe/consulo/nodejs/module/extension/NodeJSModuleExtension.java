4a131cceaa98ad10aa3c14acced4579e614c6f72
VISTALL <vistall.valeriy@gmail.com>
2015-03-19 21:04:31 +0300

    extend javascript module extension

--- src/org/mustbe/consulo/nodejs/module/extension/NodeJSModuleExtension.java
+++ src/org/mustbe/consulo/nodejs/module/extension/NodeJSModuleExtension.java
@@ -16,9 +16,9 @@
 
 package org.mustbe.consulo.nodejs.module.extension;
 
-import org.consulo.module.extension.ModuleExtensionWithSdk;
 import org.consulo.module.extension.impl.ModuleExtensionWithSdkImpl;
 import org.jetbrains.annotations.NotNull;
+import org.mustbe.consulo.javascript.module.extension.JavaScriptModuleExtension;
 import org.mustbe.consulo.nodejs.bundle.NodeJSBundleType;
 import com.intellij.openapi.projectRoots.SdkType;
 import com.intellij.openapi.roots.ModuleRootLayer;
@@ -27,7 +27,8 @@ import com.intellij.openapi.roots.ModuleRootLayer;
  * @author VISTALL
  * @since 14.03.14
  */
-public class NodeJSModuleExtension extends ModuleExtensionWithSdkImpl<NodeJSModuleExtension> implements ModuleExtensionWithSdk<NodeJSModuleExtension>
+public class NodeJSModuleExtension extends ModuleExtensionWithSdkImpl<NodeJSModuleExtension> implements
+		JavaScriptModuleExtension<NodeJSModuleExtension>
 {
 	public NodeJSModuleExtension(@NotNull String id, @NotNull ModuleRootLayer rootModel)
 	{
