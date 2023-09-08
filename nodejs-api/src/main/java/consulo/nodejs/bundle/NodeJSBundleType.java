e7cc0d0de62cdfea5f48c8bf9b65b63d6f715974
Valery Semenchuk <vistall.valeriy@gmail.com>
2023-09-08 11:18:46 +0400

    drop usage deprecated class - replace api usage

--- nodejs-api/src/main/java/consulo/nodejs/bundle/NodeJSBundleType.java
+++ nodejs-api/src/main/java/consulo/nodejs/bundle/NodeJSBundleType.java
@@ -29,8 +29,8 @@ import consulo.nodejs.icon.NodeJSApiIconGroup;
 import consulo.platform.Platform;
 import consulo.process.ExecutionException;
 import consulo.process.cmd.GeneralCommandLine;
-import consulo.process.local.ExecUtil;
-import consulo.process.local.ProcessOutput;
+import consulo.process.util.CapturingProcessUtil;
+import consulo.process.util.ProcessOutput;
 import consulo.ui.image.Image;
 import consulo.util.lang.StringUtil;
 import consulo.virtualFileSystem.LocalFileSystem;
@@ -175,7 +175,7 @@ public class NodeJSBundleType extends SdkType
 			commandLine.withWorkDirectory(s);
 			commandLine.addParameter("-v");
 
-			ProcessOutput processOutput = ExecUtil.execAndGetOutput(commandLine);
+			ProcessOutput processOutput = CapturingProcessUtil.execAndGetOutput(commandLine);
 			String stdout = processOutput.getStdout();
 			if(StringUtil.startsWith(stdout, "v"))
 			{
