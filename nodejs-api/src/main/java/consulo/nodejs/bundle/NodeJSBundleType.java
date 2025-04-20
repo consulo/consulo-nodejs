3d1ec8d3f34c7929bf5a7de8b925d8b344a7028f
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-20 16:16:47 +0400

    drop usage of EP_NAME

--- nodejs-api/src/main/java/consulo/nodejs/bundle/NodeJSBundleType.java
+++ nodejs-api/src/main/java/consulo/nodejs/bundle/NodeJSBundleType.java
@@ -17,6 +17,7 @@
 package consulo.nodejs.bundle;
 
 import consulo.annotation.component.ExtensionImpl;
+import consulo.application.Application;
 import consulo.container.plugin.PluginManager;
 import consulo.content.OrderRootType;
 import consulo.content.base.BinariesOrderRootType;
@@ -35,9 +36,9 @@ import consulo.ui.image.Image;
 import consulo.util.lang.StringUtil;
 import consulo.virtualFileSystem.LocalFileSystem;
 import consulo.virtualFileSystem.VirtualFile;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -48,170 +49,140 @@ import java.util.List;
  * @since 14.03.14
  */
 @ExtensionImpl
-public class NodeJSBundleType extends SdkType
-{
-	@Nonnull
-	public static NodeJSBundleType getInstance()
-	{
-		return EP_NAME.findExtensionOrFail(NodeJSBundleType.class);
-	}
+public class NodeJSBundleType extends SdkType {
+    @Nonnull
+    public static NodeJSBundleType getInstance() {
+        return Application.get().getExtensionPoint(SdkType.class).findExtensionOrFail(NodeJSBundleType.class);
+    }
 
-	@Nonnull
-	public static File getExePath(@Nonnull Sdk sdk)
-	{
-		String homePath = sdk.getHomePath();
-		assert homePath != null;
-		return getExePath(homePath);
-	}
+    @Nonnull
+    public static File getExePath(@Nonnull Sdk sdk) {
+        String homePath = sdk.getHomePath();
+        assert homePath != null;
+        return getExePath(homePath);
+    }
 
-	@Nonnull
-	public static File getExePath(@Nonnull String home)
-	{
-		return getExePath(home, "node.exe", "node");
-	}
+    @Nonnull
+    public static File getExePath(@Nonnull String home) {
+        return getExePath(home, "node.exe", "node");
+    }
 
-	@Nonnull
-	public static File getExePath(@Nonnull String home, @Nonnull String winName, String otherName)
-	{
-		String executable = Platform.current().os().isWindows() ? winName : otherName;
+    @Nonnull
+    public static File getExePath(@Nonnull String home, @Nonnull String winName, String otherName) {
+        String executable = Platform.current().os().isWindows() ? winName : otherName;
 
-		File firstTry = new File(home, "bin/" + executable);
-		if(firstTry.exists())
-		{
-			return firstTry;
-		}
-		return new File(home, executable);
-	}
+        File firstTry = new File(home, "bin/" + executable);
+        if (firstTry.exists()) {
+            return firstTry;
+        }
+        return new File(home, executable);
+    }
 
-	public NodeJSBundleType()
-	{
-		super("NODEJS");
-	}
+    public NodeJSBundleType() {
+        super("NODEJS");
+    }
 
-	@Nonnull
-	@Override
-	public Collection<String> suggestHomePaths()
-	{
-		List<String> paths = new ArrayList<>();
-		Platform platform = Platform.current();
-		if(platform.os().isWindows())
-		{
-			collectNodePathsAtWindows(paths, "ProgramFiles");
-			collectNodePathsAtWindows(paths, "ProgramFiles(x86)");
-		}
-		else
-		{
-			File userHome = platform.user().homePath().toFile();
+    @Nonnull
+    @Override
+    public Collection<String> suggestHomePaths() {
+        List<String> paths = new ArrayList<>();
+        Platform platform = Platform.current();
+        if (platform.os().isWindows()) {
+            collectNodePathsAtWindows(paths, "ProgramFiles");
+            collectNodePathsAtWindows(paths, "ProgramFiles(x86)");
+        }
+        else {
+            File userHome = platform.user().homePath().toFile();
 
-			File nvmHome = new File(userHome, ".nvm/versions/node");
-			if(nvmHome.exists())
-			{
-				for(File file : nvmHome.listFiles())
-				{
-					if(file.isDirectory())
-					{
-						paths.add(file.getPath());
-					}
-				}
-			}
-		}
-		return paths;
-	}
+            File nvmHome = new File(userHome, ".nvm/versions/node");
+            if (nvmHome.exists()) {
+                for (File file : nvmHome.listFiles()) {
+                    if (file.isDirectory()) {
+                        paths.add(file.getPath());
+                    }
+                }
+            }
+        }
+        return paths;
+    }
 
-	private static void collectNodePathsAtWindows(List<String> list, String env)
-	{
-		String programFiles = Platform.current().os().getEnvironmentVariable(env);
-		if(programFiles != null)
-		{
-			File nodejsPath = new File(programFiles, "nodejs");
-			if(nodejsPath.exists())
-			{
-				list.add(nodejsPath.getPath());
-			}
-		}
-	}
+    private static void collectNodePathsAtWindows(List<String> list, String env) {
+        String programFiles = Platform.current().os().getEnvironmentVariable(env);
+        if (programFiles != null) {
+            File nodejsPath = new File(programFiles, "nodejs");
+            if (nodejsPath.exists()) {
+                list.add(nodejsPath.getPath());
+            }
+        }
+    }
 
-	@Override
-	public void setupSdkPaths(Sdk sdk)
-	{
-		SdkModificator modificator = sdk.getSdkModificator();
+    @Override
+    public void setupSdkPaths(Sdk sdk) {
+        SdkModificator modificator = sdk.getSdkModificator();
 
-		VirtualFile stubsDirectory = LocalFileSystem.getInstance().findFileByIoFile(new File(PluginManager.getPluginPath(NodeJSBundleType.class), "stubs"));
-		if(stubsDirectory != null)
-		{
-			for(VirtualFile file : stubsDirectory.getChildren())
-			{
-				if(file.getFileType() == JavaScriptFileType.INSTANCE)
-				{
-					modificator.addRoot(file, BinariesOrderRootType.getInstance());
-					modificator.addRoot(file, SourcesOrderRootType.getInstance());
-				}
-			}
-		}
+        VirtualFile stubsDirectory = LocalFileSystem.getInstance().findFileByIoFile(new File(PluginManager.getPluginPath(NodeJSBundleType.class), "stubs"));
+        if (stubsDirectory != null) {
+            for (VirtualFile file : stubsDirectory.getChildren()) {
+                if (file.getFileType() == JavaScriptFileType.INSTANCE) {
+                    modificator.addRoot(file, BinariesOrderRootType.getInstance());
+                    modificator.addRoot(file, SourcesOrderRootType.getInstance());
+                }
+            }
+        }
 
-		modificator.commitChanges();
-	}
+        modificator.commitChanges();
+    }
 
-	@Override
-	public boolean canCreatePredefinedSdks()
-	{
-		return true;
-	}
+    @Override
+    public boolean canCreatePredefinedSdks() {
+        return true;
+    }
 
-	@Override
-	public boolean isValidSdkHome(String sdkHome)
-	{
-		return getExePath(sdkHome).exists();
-	}
+    @Override
+    public boolean isValidSdkHome(String sdkHome) {
+        return getExePath(sdkHome).exists();
+    }
 
-	@Nullable
-	@Override
-	public String getVersionString(String s)
-	{
-		try
-		{
-			GeneralCommandLine commandLine = new GeneralCommandLine();
-			commandLine.setExePath(getExePath(s).getPath());
-			commandLine.withWorkDirectory(s);
-			commandLine.addParameter("-v");
+    @Nullable
+    @Override
+    public String getVersionString(String s) {
+        try {
+            GeneralCommandLine commandLine = new GeneralCommandLine();
+            commandLine.setExePath(getExePath(s).getPath());
+            commandLine.withWorkDirectory(s);
+            commandLine.addParameter("-v");
 
-			ProcessOutput processOutput = CapturingProcessUtil.execAndGetOutput(commandLine);
-			String stdout = processOutput.getStdout();
-			if(StringUtil.startsWith(stdout, "v"))
-			{
-				stdout = stdout.substring(1, stdout.length());
-			}
-			return stdout.trim();
-		}
-		catch(ExecutionException e)
-		{
-			return null;
-		}
-	}
+            ProcessOutput processOutput = CapturingProcessUtil.execAndGetOutput(commandLine);
+            String stdout = processOutput.getStdout();
+            if (StringUtil.startsWith(stdout, "v")) {
+                stdout = stdout.substring(1, stdout.length());
+            }
+            return stdout.trim();
+        }
+        catch (ExecutionException e) {
+            return null;
+        }
+    }
 
-	@Override
-	public boolean isRootTypeApplicable(OrderRootType type)
-	{
-		return type == BinariesOrderRootType.getInstance() || type == SourcesOrderRootType.getInstance();
-	}
+    @Override
+    public boolean isRootTypeApplicable(OrderRootType type) {
+        return type == BinariesOrderRootType.getInstance() || type == SourcesOrderRootType.getInstance();
+    }
 
-	@Override
-	public String suggestSdkName(String s, String sdkHome)
-	{
-		return "nodejs " + getVersionString(sdkHome);
-	}
+    @Override
+    public String suggestSdkName(String s, String sdkHome) {
+        return "nodejs " + getVersionString(sdkHome);
+    }
 
-	@Nonnull
-	@Override
-	public String getPresentableName()
-	{
-		return "NodeJS";
-	}
+    @Nonnull
+    @Override
+    public String getPresentableName() {
+        return "NodeJS";
+    }
 
-	@Nullable
-	@Override
-	public Image getIcon()
-	{
-		return NodeJSApiIconGroup.nodejs();
-	}
+    @Nonnull
+    @Override
+    public Image getIcon() {
+        return NodeJSApiIconGroup.nodejs();
+    }
 }
