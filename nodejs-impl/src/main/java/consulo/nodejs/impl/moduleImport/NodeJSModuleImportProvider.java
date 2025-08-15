ae9186cfa7596807f0b7e3e35340757aa3818082
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-08-15 18:51:46 +0400

    migrate to localize value

--- nodejs-impl/src/main/java/consulo/nodejs/impl/moduleImport/NodeJSModuleImportProvider.java
+++ nodejs-impl/src/main/java/consulo/nodejs/impl/moduleImport/NodeJSModuleImportProvider.java
@@ -5,6 +5,7 @@ import consulo.annotation.component.ExtensionImpl;
 import consulo.application.WriteAction;
 import consulo.content.base.ExcludedContentFolderTypeProvider;
 import consulo.ide.moduleImport.ModuleImportProvider;
+import consulo.localize.LocalizeValue;
 import consulo.module.ModifiableModuleModel;
 import consulo.module.Module;
 import consulo.module.content.ModuleRootManager;
@@ -18,9 +19,9 @@ import consulo.ui.ex.wizard.WizardStep;
 import consulo.ui.image.Image;
 import consulo.virtualFileSystem.LocalFileSystem;
 import consulo.virtualFileSystem.VirtualFile;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import java.io.File;
 import java.util.function.Consumer;
 
@@ -29,66 +30,59 @@ import java.util.function.Consumer;
  * @since 05/12/2020
  */
 @ExtensionImpl
-public class NodeJSModuleImportProvider implements ModuleImportProvider<NodeJSModuleImportContext>
-{
-	@Nonnull
-	@Override
-	public String getName()
-	{
-		return "Node.js";
-	}
+public class NodeJSModuleImportProvider implements ModuleImportProvider<NodeJSModuleImportContext> {
+    @Nonnull
+    @Override
+    public LocalizeValue getName() {
+        return LocalizeValue.localizeTODO("Node.js");
+    }
 
-	@Nonnull
-	@Override
-	public Image getIcon()
-	{
-		return NodeJSApiIconGroup.nodejs();
-	}
+    @Nonnull
+    @Override
+    public Image getIcon() {
+        return NodeJSApiIconGroup.nodejs();
+    }
 
-	@Override
-	public boolean canImport(@Nonnull File file)
-	{
-		return new File(file, "package.json").exists();
-	}
+    @Override
+    public boolean canImport(@Nonnull File file) {
+        return new File(file, "package.json").exists();
+    }
 
-	@Nonnull
-	@Override
-	public NodeJSModuleImportContext createContext(@Nullable Project project)
-	{
-		return new NodeJSModuleImportContext(project);
-	}
+    @Nonnull
+    @Override
+    public NodeJSModuleImportContext createContext(@Nullable Project project) {
+        return new NodeJSModuleImportContext(project);
+    }
 
-	@Override
-	public void buildSteps(@Nonnull Consumer<WizardStep<NodeJSModuleImportContext>> consumer, @Nonnull NodeJSModuleImportContext context)
-	{
-		consumer.accept(new NodeJSNewModuleSetupStep<>(context));
-	}
+    @Override
+    public void buildSteps(@Nonnull Consumer<WizardStep<NodeJSModuleImportContext>> consumer, @Nonnull NodeJSModuleImportContext context) {
+        consumer.accept(new NodeJSNewModuleSetupStep<>(context));
+    }
 
-	@RequiredReadAction
-	@Override
-	public void process(@Nonnull NodeJSModuleImportContext context,
-						@Nonnull Project project,
-						@Nonnull ModifiableModuleModel modifiableModuleModel,
-						@Nonnull Consumer<Module> consumer)
-	{
-		File dir = new File(context.getPath());
+    @RequiredReadAction
+    @Override
+    public void process(@Nonnull NodeJSModuleImportContext context,
+                        @Nonnull Project project,
+                        @Nonnull ModifiableModuleModel modifiableModuleModel,
+                        @Nonnull Consumer<Module> consumer) {
+        File dir = new File(context.getPath());
 
-		VirtualFile localDir = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(dir);
+        VirtualFile localDir = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(dir);
 
-		Module module = modifiableModuleModel.newModule(dir.getName(), dir.getPath());
+        Module module = modifiableModuleModel.newModule(dir.getName(), dir.getPath());
 
-		ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
+        ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
 
-		ModifiableRootModel modifiableModel = moduleRootManager.getModifiableModel();
+        ModifiableRootModel modifiableModel = moduleRootManager.getModifiableModel();
 
-		ContentEntry contentEntry = modifiableModel.addContentEntry(localDir);
+        ContentEntry contentEntry = modifiableModel.addContentEntry(localDir);
 
-		contentEntry.addFolder(localDir.getUrl() + "/node_modules", ExcludedContentFolderTypeProvider.getInstance());
+        contentEntry.addFolder(localDir.getUrl() + "/node_modules", ExcludedContentFolderTypeProvider.getInstance());
 
-		NodeJSMutableModuleExtension extension = modifiableModel.getExtensionWithoutCheck(NodeJSMutableModuleExtension.class);
-		extension.setEnabled(true);
-		extension.getInheritableSdk().set(null, context.getSdk());
+        NodeJSMutableModuleExtension extension = modifiableModel.getExtensionWithoutCheck(NodeJSMutableModuleExtension.class);
+        extension.setEnabled(true);
+        extension.getInheritableSdk().set(null, context.getSdk());
 
-		WriteAction.runAndWait(modifiableModel::commit);
-	}
+        WriteAction.runAndWait(modifiableModel::commit);
+    }
 }
