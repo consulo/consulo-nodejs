088546c58cc924848e1d8398401b4aff8e2b2874
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-02-15 22:09:32 +0200

    add first resume

--- nodejs-impl/src/main/java/consulo/nodejs/impl/run/NodeJSDebugProcess.java
+++ nodejs-impl/src/main/java/consulo/nodejs/impl/run/NodeJSDebugProcess.java
@@ -1,5 +1,6 @@
 package consulo.nodejs.impl.run;
 
+import com.github.kklisura.cdt.protocol.events.debugger.Paused;
 import consulo.execution.ExecutionResult;
 import consulo.execution.debug.XDebugSession;
 import consulo.javascript.debugger.cdt.CDTProcessBase;
@@ -11,7 +12,20 @@ import jakarta.annotation.Nonnull;
  * @since 2026-02-15
  */
 public class NodeJSDebugProcess extends CDTProcessBase {
+    private boolean myFirstPause = true;
+
     public NodeJSDebugProcess(@Nonnull XDebugSession session, ExecutionResult result) throws ExecutionException {
         super(session, result);
+    }
+
+    @Override
+    protected void onPause(Paused event) {
+        if (myFirstPause) {
+            myFirstPause = false;
+            invoke(devTools -> devTools.getDebugger().resume());
+            return;
+        }
+
+        super.onPause(event);
     }
 }
